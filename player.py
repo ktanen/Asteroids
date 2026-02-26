0b53ff347707edf345d1272eb72c283b93a6c185
ktanen <kevinbtanen@gmail.com>
2026-02-26 10:01:59 -0500

    Added ability to shoot. Does not do anything yet.

--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 import pygame 
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, SHOT_RADIUS
+from shot import Shot
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
@@ -35,6 +36,9 @@ class Player(CircleShape):
         if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
             self.rotate(dt)
         
+        # Shooting
+        if keys[pygame.K_SPACE]:
+            self.shoot()
 
 
     def move(self, dt):
@@ -42,3 +46,10 @@ class Player(CircleShape):
         rotated_vector = unit_vector.rotate(self.rotation)
         rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
         self.position += rotated_with_speed_vector
+    
+    def shoot(self):
+        bullet = Shot(self.position.x, self.position.y)
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        scaled_vector = rotated_vector * PLAYER_SHOOT_SPEED
+        bullet.velocity = scaled_vector
