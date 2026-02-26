f781432832d4a1f409c0e488f40b39f70c3e139b
ktanen <kevinbtanen@gmail.com>
2026-02-26 09:24:50 -0500

    Added asteroids (no collision yet)

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -9,7 +12,12 @@ def main():
     dt = 0
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    asteroidField = AsteroidField()
     ship = Player(SCREEN_WIDTH /2, SCREEN_HEIGHT /2)
 
 
@@ -18,7 +26,7 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-                
+
         updatable.update(dt)
         screen.fill("black")
         for image in drawable:
