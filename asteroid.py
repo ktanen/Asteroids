b66ed84f240b70f5cbdfaab58263c228ee4e50fa
ktanen <kevinbtanen@gmail.com>
2026-02-26 10:27:00 -0500

    Asteroids now split when shot instead of just disappearing.

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,9 @@
 import pygame
+import random
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
+from logger import log_event
+
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
@@ -10,3 +13,27 @@ class Asteroid(CircleShape):
     
     def update(self, dt):
         self.position += (self.velocity * dt)
+
+    def split(self):
+        
+        #The shot asteroid always disapepars
+        self.kill()
+        
+        # Small asteroids disappear without spawning new ones
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        # Medium and large asteroids split into two
+        # asteroids that are one size smaller than the original
+
+        log_event("asteroid_split")
+        angle = random.uniform(20, 50)
+        first_new_direction = self.velocity.rotate(angle)
+        second_new_direction = self.velocity.rotate(-angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        first_smaller_asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        second_smaller_asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        first_smaller_asteroid.velocity = (1.2 * first_new_direction)
+        second_smaller_asteroid.velocity = (1.2 * second_new_direction)
+        
+        
--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def main():
             for shot in shots:
                 if shot.collides_with(asteroid):
                     log_event("asteroid_shot")
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
 
 
