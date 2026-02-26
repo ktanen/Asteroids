c5823ba0fa4c6747de4e65f9e785931368b000b7
ktanen <kevinbtanen@gmail.com>
2026-02-26 10:17:54 -0500

    Asteroids now disappear when shot

--- main.py
+++ main.py
@@ -35,6 +35,14 @@ def main():
         updatable.update(dt)
 
         for asteroid in asteroids:
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    asteroid.kill()
+                    shot.kill()
+
+
+
             if asteroid.collides_with(ship):
                 log_event("player_hit")
                 print("Game over!")
